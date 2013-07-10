072e3a0af389cf8b562a2fa070f78e2591027de8
DerManoMann <mano@zenmagick.org>
2013-07-10 15:29:05 +1200

    Inital version

---
+++ tests/ACache/Tests/NamespaceCacheTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace ACache\Tests;
+
+use ACache\Cache;
+use ACache\ArrayCache;
+use ACache\NamespaceCache;
+
+/**
+ * NamespaceCache tests
+ */
+class NamespaceCacheTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Test namespace.
+     */
+    public function testNamespace()
+    {
+        // regular
+        $this->doTestNamespace(new ArrayCache());
+
+        // nested
+        $this->doTestNamespace(new NamespaceCache(new ArrayCache(), 'super'));
+    }
+
+    /**
+     * Do namespace tests.
+     */
+    protected function doTestNamespace(Cache $decoratedCache)
+    {
+        $cache = new NamespaceCache($decoratedCache, 'foo');
+        $this->assertFalse($cache->contains('ying'));
+        $this->assertNull($cache->fetch('ying'));
+
+        $this->assertTrue($cache->save('ying', 'yang'));
+        $this->assertTrue($cache->contains('ying'));
+        $this->assertEquals('yang', $cache->fetch('ying'));
+        // check decorated cache to make sure there is a namespace in the name...
+        $this->assertFalse($decoratedCache->contains('ying'));
+
+        $this->assertTrue($cache->delete('ying'));
+        $this->assertFalse($cache->contains('ying'));
+        $this->assertNull($cache->fetch('ying'));
+
+        $stats = $decoratedCache->getStats();
+        $this->assertEquals(0, $stats[Cache::STATS_SIZE]);
+
+        $this->assertTrue($cache->save('foo', 'bar'));
+        $stats = $decoratedCache->getStats();
+        $this->assertEquals(1, $stats[Cache::STATS_SIZE]);
+
+        $cache->flush();
+        $stats = $decoratedCache->getStats();
+        $this->assertFalse($cache->contains('foo'));
+        $this->assertEquals(0, $stats[Cache::STATS_SIZE]);
+    }
+
+    /**
+     * Test empty namespace.
+     */
+    public function testEmptyNamespace()
+    {
+        // regular
+        $this->doTestEmptyNamespace(new ArrayCache());
+
+        // nested
+        $this->doTestEmptyNamespace(new NamespaceCache(new ArrayCache(), 'super'));
+    }
+
+    /**
+     * Do empty namespace tests.
+     */
+    protected function doTestEmptyNamespace(Cache $decoratedCache)
+    {
+        $cache = new NamespaceCache($decoratedCache);
+        $this->assertFalse($cache->contains('ying'));
+        $this->assertNull($cache->fetch('ying'));
+
+        $this->assertTrue($cache->save('ying', 'yang'));
+        $this->assertTrue($cache->contains('ying'));
+        $this->assertEquals('yang', $cache->fetch('ying'));
+        // check decorated cache as that should be the same
+        $this->assertTrue($decoratedCache->contains('ying'));
+
+        $cache->flush();
+        $stats = $decoratedCache->getStats();
+        $this->assertFalse($cache->contains('foo'));
+        $this->assertEquals(0, $stats[Cache::STATS_SIZE]);
+    }
+
+}
