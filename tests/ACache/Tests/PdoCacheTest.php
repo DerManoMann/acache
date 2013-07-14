916d86db75069e7f43a83e8017cd8232df9c4d14
DerManoMann <mano@radebatz.net>
2013-07-14 17:46:55 +1200

    Add Pdo db config tests

--- tests/ACache/Tests/PdoCacheTest.php
+++ tests/ACache/Tests/PdoCacheTest.php
@@ -15,17 +15,32 @@ class PdoCacheTest extends NamespaceCacheTest
      */
     public function cacheProvider()
     {
-        $pdo = new PDO('sqlite::memory:');
+        $pdoDefaults = new PDO('sqlite::memory:');
         // create cache table using naming defaults
-        $stmt = $pdo->prepare('CREATE TABLE cache(
+        $stmt = $pdoDefaults->prepare('CREATE TABLE cache(
             id      char(64) PRIMARY KEY NOT NULL,
             entry   TEXT NOT NULL,
             expires INT DEFAULT 0
         );');
         $stmt->execute();
 
+        $pdoCustom = new PDO('sqlite::memory:');
+        // create cache table using custom config
+        $stmt = $pdoCustom->prepare('CREATE TABLE ccache(
+            cid      char(64) PRIMARY KEY NOT NULL,
+            centry   TEXT NOT NULL,
+            cexpires INT DEFAULT 0
+        );');
+        $stmt->execute();
+
         return array(
-            array(new PdoCache($pdo))
+            array(new PdoCache($pdoDefaults)),
+            array(new PdoCache($pdoCustom, array(
+                't_cache' => 'ccache',
+                'c_id' => 'cid',
+                'c_entry' => 'centry',
+                'c_expires' => 'cexpires',
+            ))),
         );
     }
 
