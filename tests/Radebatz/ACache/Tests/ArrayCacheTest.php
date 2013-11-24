11dd74a971a271004fb9195742a8847568094f8f
DerManoMann <mano@radebatz.net>
2013-11-25 10:50:37 +1300

    issue #5: add missing licence headers

--- tests/Radebatz/ACache/Tests/ArrayCacheTest.php
+++ tests/Radebatz/ACache/Tests/ArrayCacheTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+* This file is part of the ACache library.
+*
+* (c) Martin Rademacher <mano@radebatz.net>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
 namespace Radebatz\ACache\Tests;
 
 use Radebatz\ACache\ArrayCache;
