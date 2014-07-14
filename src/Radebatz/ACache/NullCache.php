4d2c55c00b35ad6acb8c11ba547f9113e4e030e9
DerManoMann <mano@radebatz.net>
2014-07-14 16:15:34 +1200

    Resolves #13 - Add NullCache

---
+++ src/Radebatz/ACache/NullCache.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+* This file is part of the ACache library.
+*
+* (c) Martin Rademacher <mano@radebatz.net>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Radebatz\ACache;
+
+/**
+ * Null cache.
+ *
+ * @author Martin Rademacher <mano@radebatz.net>
+ */
+class NullCache implements CacheInterface
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function fetch($id, $namespace = null)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function contains($id, $namespace = null)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTimeToLive($id, $namespace = null) {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function save($id, $entry, $lifeTime = 0, $namespace = null)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function delete($id, $namespace = null)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function flush($namespace = null)
+    {
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getStats()
+    {
+        return array(
+            CacheInterface::STATS_SIZE => 0,
+        );
+    }
+
+}
---
+++ tests/Radebatz/ACache/Tests/NullCacheTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+* This file is part of the ACache library.
+*
+* (c) Martin Rademacher <mano@radebatz.net>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Radebatz\ACache\Tests;
+
+use Radebatz\ACache\NullCache;
+
+/**
+ * NullCache tests.
+ */
+class NullCacheTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Simple testing.
+     */
+    public function testSimple()
+    {
+        $cache = new NullCache();
+        $this->assertTrue($cache->save('foo', 'bar'));
+        $this->assertFalse($cache->contains('foo'));
+        $this->assertTrue($cache->delete('foo'));
+        $this->assertFalse($cache->contains('foo'));
+    }
+
+}
