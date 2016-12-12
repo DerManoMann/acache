0b4d8bcbb94f973e49bbb61ad4f9d35c7cff8de1
DerManoMann <mano@radebatz.net>
2016-12-13 12:10:07 +1300

    Compare null using ===

--- src/Radebatz/ACache/Decorators/Psr/CacheItem.php
+++ src/Radebatz/ACache/Decorators/Psr/CacheItem.php
@@ -75,7 +75,7 @@ class CacheItem implements CacheItemInterface
     {
         // underlying cache
         $cache = $this->cacheItemPool->getCache();
-        $notStale = null == $this->expiresAt || time() < $this->expiresAt->getTimestamp();
+        $notStale = null === $this->expiresAt || time() < $this->expiresAt->getTimestamp();
 
         return ($cache->contains($this->key) || $this->cacheItemPool->isDeferred($this->key)) && $notStale;
     }
