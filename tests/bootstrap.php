e67e6685ff646e8c961c3a64199ba00ace20aa31
DerManoMann <mano@radebatz.net>
2018-09-20 08:42:24 +1200

    Drop 7.3 again, try workaround for phpunit namespaces

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,23 +9,27 @@
 * file that was distributed with this source code.
 */
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->add('Radebatz\ACache\Tests', __DIR__);
+namespace {
+    $loader = require __DIR__ . '/../vendor/autoload.php';
+    $loader->add('Radebatz\ACache\Tests', __DIR__);
 
-// add custom ini setting if file exists
-if (file_exists(dirname(__DIR__) . '/phpunit.ini')) {
-    $settings = parse_ini_file(dirname(__DIR__) . '/phpunit.ini', true);
-    foreach ($settings as $group => $kvp) {
-        foreach ($kvp as $key => $value) {
-            $dKey = strtoupper(sprintf('phpunit_%s_%s', $group, $key));
-            echo 'Default override: ' . $dKey . ' = ' . $value . "\n";
-            define($dKey, $value);
+    // add custom ini setting if file exists
+    if (file_exists(dirname(__DIR__) . '/phpunit.ini')) {
+        $settings = parse_ini_file(dirname(__DIR__) . '/phpunit.ini', true);
+        foreach ($settings as $group => $kvp) {
+            foreach ($kvp as $key => $value) {
+                $dKey = strtoupper(sprintf('phpunit_%s_%s', $group, $key));
+                echo 'Default override: ' . $dKey . ' = ' . $value . "\n";
+                define($dKey, $value);
+            }
         }
     }
 }
 
-if (!class_exists('PHPUnit_Framework_TestCase')) {
-    class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
-    {
+namespace PHPUnit\Framework {
+    if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+        class TestCase extends \PHPUnit_Framework_TestCase
+        {
+        }
     }
 }
